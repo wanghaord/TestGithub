2e30e293c1e72807b29a06c98aa6cd0e649a0956
admin <1054076455@qq.com>
2019-04-24 17:11:37 +0800

    修改了类2

--- src/com/hao/Test2.java
+++ src/com/hao/Test2.java
@@ -1,4 +1,4 @@
-package com.hao;
+﻿package com.hao;
 
 /**
  * @author wanghao
@@ -6,6 +6,7 @@ package com.hao;
  */
 public class Test2 {
     public static void main(String[] args) {
-        System.out.println("这是第二个测试类");
+        System.out.println("这是第二个测试类sd");
+
     }
 }
