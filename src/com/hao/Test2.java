eb97e0a86c4ee0ad4120c5f3ed1c75f151af9aea
admin <1054076455@qq.com>
2019-04-24 16:40:34 +0800

    test github

---
+++ src/com/hao/Test2.java
@@ -0,0 +1,11 @@
+package com.hao;
+
+/**
+ * @author wanghao
+ * @date 2019/4/24.
+ */
+public class Test2 {
+    public static void main(String[] args) {
+        System.out.println("这是第二个测试类");
+    }
+}
---
+++ src/com/runqing/Test1.java
@@ -0,0 +1,12 @@
+package com.runqing;
+
+/**
+ * @author wanghao
+ * @date 2019/4/24.
+ */
+public class Test1 {
+
+    public static void main(String[] args) {
+        System.out.println("这是第一个测试类");
+    }
+}
